8983655a9f94db929df8e317fefa872c614783d1
Jose Lameiras <joselameiras@gmail.com>
2022-08-10 14:08:51 -0500

    se agrego una vista para mostrar los registros en una tabla

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,11 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cursos en linea</title>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+ 
 </head>
-<body>
+<body class="table">
     <ul>
-<li><a href="{{url("/courses")}}">todos</a></li>
+<li><a href="{{url("/courses")}}">Todos</a></li>
 <li><a href="{{url("/courses/create")}}">Nuevo</a></li>
     </ul>
+@yield('content')
 </body>
 </html>
