6516a436b1927cb596a749f9412bcdfb17c94ceb
Jose Lameiras <joselameiras@gmail.com>
2022-08-10 13:11:04 -0500

    menu de cursos

---
+++ resources/views/main.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cursos en linea</title>
+</head>
+<body>
+    <ul>
+<li><a href="{{url("/courses")}}">todos</a></li>
+<li><a href="{{url("/courses/create")}}">Nuevo</a></li>
+    </ul>
+</body>
+</html>
